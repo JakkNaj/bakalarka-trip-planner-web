364f6e0e90f4b0d7097493f2797f03718f1f2fb1
Jakub Najman <jakub@najman.cz>
2024-08-22 22:04:27 +0200

    colors, fonts, header styled

---
+++ src/assets/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    mainBlue: "#192bc2",
+    backgroundGrey: "#eeedea",
+    headerGrey: "#e3e0db",
+    headingText: "#2b2b2e",
+    normalText: "#535355",
+}
+
--- src/components/LayoutComponent.tsx
+++ src/components/LayoutComponent.tsx
@@ -1,24 +1,80 @@
 import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+import {fonts} from "../assets/fonts.ts";
+import {colors} from "../assets/colors.ts";
+import {Avatar as MuiAvatar} from "@mui/material";
+import {KeyboardArrowDown} from "@mui/icons-material";
+import {useStore} from "../stores/globalStore.ts";
+import logoIpsum from "../assets/logoIpsum.svg";
 
 export const LayoutComponent = () => {
+    const { user } = useStore();
+    const avatarUrl = user?.avatar_url;
 
     return (
         <>
-            <nav style={{ display: "flex", justifyContent: "space-around"}}>
+            <Styled.Nav>
+                <Styled.LogoLink to="/home">
+                    <Styled.Logo src={logoIpsum} alt="Logo" />
+                </Styled.LogoLink>
                 <p>
-                    <Link to="/">home</Link>
+                    <Styled.NavLink to="/" className="active">upcoming trips</Styled.NavLink>
+                    <Styled.Separator>|</Styled.Separator>
+                    <Styled.NavLink to="/">previous trips</Styled.NavLink>
                 </p>
-                <p>
-                    <Link to="/">upcoming trips</Link>
-                </p>
-                <p>
-                    <Link to="/">previous trips</Link>
-                </p>
-                <p>profile icon placeholder</p>
-            </nav>
+                <Styled.Profile>
+                    <Styled.ArrowDown />
+                    <MuiAvatar src={avatarUrl} alt="User Avatar" />
+                </Styled.Profile>
+            </Styled.Nav>
             <main>
                 <Outlet/>
             </main>
         </>
     );
 }
+
+const Styled = {
+    Nav: styled.nav({
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: colors.headerGrey,
+        padding: "0.4rem 0",
+    }),
+    NavLink: styled(Link)({
+        fontFamily: fonts.heading,
+        color: colors.mainBlue,
+        fontSize: "1rem",
+        textDecoration: "none",
+        padding: "0.4rem",
+        "&.active": {
+            borderBottom: `1px solid ${colors.mainBlue}`,
+        },
+        "&:hover": {
+            color: colors.mainBlue,
+        },
+    }),
+    Separator: styled.span({
+        margin: "0 1.4rem",
+        color: colors.mainBlue,
+    }),
+    LogoLink: styled(Link)({
+        display: "flex",
+        alignItems: "center",
+    }),
+    Logo: styled.img({
+        height: "20px",
+        marginRight: "1rem",
+    }),
+    Profile: styled.div({
+        display: "flex",
+        alignItems: "center",
+        cursor: "pointer",
+    }),
+    ArrowDown: styled(KeyboardArrowDown)({
+        padding: "0.2rem",
+        color: colors.mainBlue,
+    }),
+}
+
